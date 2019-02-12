20f97535c2745541070ed9f7408e88b13a59be21
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-02-12 15:12:52 +0000

    feat: added method to add billion seconds to given date

--- src/main/java/Gigasecond.java
+++ src/main/java/Gigasecond.java
@@ -3,16 +3,24 @@ import java.time.LocalDateTime;
 
 class Gigasecond {
 
+    public LocalDateTime dateTime;
+    public LocalDateTime dateTimePlusGigasecond;
+
     Gigasecond(LocalDate birthDate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      dateTime = LocalDateTime.of(birthDate.getYear(), birthDate.getMonth(), birthDate.getDayOfMonth(), 00, 00, 00);
+      System.out.println("111 dateTime is: " + dateTime);
     }
 
     Gigasecond(LocalDateTime birthDateTime) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      dateTime = birthDateTime;
+      System.out.println("222 dateTime is: " + dateTime);
+
     }
 
     LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      dateTimePlusGigasecond = dateTime.plusDays(11574).plusHours(1).plusMinutes(46).plusSeconds(40);
+      System.out.println("333 dateTimePlusGigasecond plus is: " + dateTimePlusGigasecond);
+      return dateTimePlusGigasecond;
     }
 
 }
