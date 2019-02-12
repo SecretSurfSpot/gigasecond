14d5863ca5f5386499dca6215709275d244f0984
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-02-12 15:13:53 +0000

    refactor: cleaned up code, removing System.out.println statements

--- src/main/java/Gigasecond.java
+++ src/main/java/Gigasecond.java
@@ -8,18 +8,14 @@ class Gigasecond {
 
     Gigasecond(LocalDate birthDate) {
       dateTime = LocalDateTime.of(birthDate.getYear(), birthDate.getMonth(), birthDate.getDayOfMonth(), 00, 00, 00);
-      System.out.println("111 dateTime is: " + dateTime);
     }
 
     Gigasecond(LocalDateTime birthDateTime) {
       dateTime = birthDateTime;
-      System.out.println("222 dateTime is: " + dateTime);
-
     }
 
     LocalDateTime getDateTime() {
       dateTimePlusGigasecond = dateTime.plusDays(11574).plusHours(1).plusMinutes(46).plusSeconds(40);
-      System.out.println("333 dateTimePlusGigasecond plus is: " + dateTimePlusGigasecond);
       return dateTimePlusGigasecond;
     }
 
