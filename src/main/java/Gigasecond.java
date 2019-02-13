a4864f530f5eb942a053b82d8903183cd6d9011d
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-02-13 16:20:15 +0000

    refactor: defined static final (constant) GIGASECOND = 1000000000

--- src/main/java/Gigasecond.java
+++ src/main/java/Gigasecond.java
@@ -5,6 +5,7 @@ class Gigasecond {
 
     public LocalDateTime dateTime;
     public LocalDateTime dateTimePlusGigasecond;
+    public static final int GIGASECOND = 1000000000;
 
     Gigasecond(LocalDate birthDate) {
       dateTime = LocalDateTime.of(birthDate.getYear(), birthDate.getMonth(), birthDate.getDayOfMonth(), 00, 00, 00);
@@ -16,7 +17,7 @@ class Gigasecond {
 
     LocalDateTime getDateTime() {
       // return dateTimePlusGigasecond = dateTime.plusDays(11574).plusHours(1).plusMinutes(46).plusSeconds(40);
-      return dateTimePlusGigasecond = dateTime.plusSeconds(1000000000);
+      return dateTimePlusGigasecond = dateTime.plusSeconds(GIGASECOND);
     }
 
 }
