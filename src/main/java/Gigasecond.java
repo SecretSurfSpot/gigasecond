f9dfa2d5d36e0a83254a993fab4ec9c7093616fd
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-02-13 16:35:18 +0000

    refactor: simplied LocalDateTime method return value

--- src/main/java/Gigasecond.java
+++ src/main/java/Gigasecond.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 class Gigasecond {
 
     public LocalDateTime dateTime;
-    public LocalDateTime dateTimePlusGigasecond;
     public static final int GIGASECOND = 1000000000;
 
     Gigasecond(LocalDate birthDate) {
@@ -16,8 +15,7 @@ class Gigasecond {
     }
 
     LocalDateTime getDateTime() {
-      // return dateTimePlusGigasecond = dateTime.plusDays(11574).plusHours(1).plusMinutes(46).plusSeconds(40);
-      return dateTimePlusGigasecond = dateTime.plusSeconds(GIGASECOND);
+      return dateTime.plusSeconds(GIGASECOND);
     }
 
 }
